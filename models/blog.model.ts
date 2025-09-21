ab90be1718b482b99b05084f2391a5e0d54d3a70
athulcoder <athul.sin.cos.tan@gmail.com>
2025-09-21 15:53:05 +0530

    blog model half

---
+++ models/blog.model.ts
@@ -0,0 +1,9 @@
+
+import mongoose from "mongoose"
+
+const blogSchema = new mongoose.Schema({
+    title:{type:String, required:true},
+    description:{type:String, required:true},
+
+},
+{timestamps:true});
