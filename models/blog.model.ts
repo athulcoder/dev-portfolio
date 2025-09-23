123d1445b69046dea37ee0c23dacbebb67d0969b
athulcoder <athul.sin.cos.tan@gmail.com>
2025-09-23 10:08:35 +0530

    new feature

--- models/blog.model.ts
+++ models/blog.model.ts
@@ -2,8 +2,13 @@
 import mongoose from "mongoose"
 
 const blogSchema = new mongoose.Schema({
-    title:{type:String, required:true},
-    description:{type:String, required:true},
+    title: { type: String, required: true },
+    description: { type: String, required: true },
 
 },
-{timestamps:true});
+
+
+    { timestamps: true });
+
+
+
