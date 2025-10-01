3d81808d97fdb03130d1c9df8de5929fd1d3bea3
athulcoder <athul.sin.cos.tan@gmail.com>
2025-10-01 18:14:44 +0530

    new

--- models/blog.model.ts
+++ models/blog.model.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
+    images:[{type:String, default:""}]
 
 },
 
