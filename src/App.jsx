19ae329f06e98c2f68152da368a7cf65d00e4697
athulcoder <athul.sin.cos.tan@gmail.com>
2025-11-01 12:35:34 +0530

    hero text ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
-    <main>
-      <div> welcome to my 3D portfolio website</div>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 };
 
