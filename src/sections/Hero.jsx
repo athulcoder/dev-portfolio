1e24c5b6df90bb543c5ffe3b0a15d0dab9c960ae
athulcoder <athul.sin.cos.tan@gmail.com>
2025-11-01 12:58:12 +0530

    button component created

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { words } from "../constants";
+import Button from "../components/Button";
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -34,8 +35,19 @@ const Hero = () => {
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none ">
+              Hi, I am Athul Sabu, a developer based in India with passion for
+              code.
+            </p>
+            <Button
+              className="md:w-72 md:h-16 w-60 h-12"
+              id="button"
+              text="See my Work"
+            />
           </div>
         </header>
+
+        {/* RIGHT SIDE : 3D MODEL */}
       </div>
     </section>
   );
