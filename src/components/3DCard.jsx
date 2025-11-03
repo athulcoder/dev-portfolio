fb8216f8d81003153512982135acbfb673dc50fa
athulcoder <athul.sin.cos.tan@gmail.com>
2025-11-03 11:03:48 +0530

    preview url show if there is preview of work

--- src/components/3DCard.jsx
+++ src/components/3DCard.jsx
@@ -6,7 +6,7 @@ import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 export function ProjectCard({ title, desc, imgPath, githubUrl, previewUrl }) {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className=" relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2]  w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -30,21 +30,25 @@ export function ProjectCard({ title, desc, imgPath, githubUrl, previewUrl }) {
           />
         </CardItem>
         <div className="flex justify-between items-center mt-20">
-          <CardItem
-            translateZ={20}
-            as="a"
-            href={previewUrl}
-            target="__blank"
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-          >
-            Preview
-          </CardItem>
+          {previewUrl ? (
+            <CardItem
+              translateZ={20}
+              as="a"
+              href={previewUrl}
+              target="__blank"
+              className="px-4 py-2 rounded-xl text-xs font-normal text-white bg-cyan-950 "
+            >
+              Preview
+            </CardItem>
+          ) : (
+            ""
+          )}
           <CardItem
             translateZ={20}
             as="a"
             href={githubUrl}
             target="__blank"
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+            className="px-4 py-2 rounded-xl  bg-white text-black  text-xs font-bold"
           >
             GitHub
           </CardItem>
