279ef1df66701be4863cd86aa88c2d0578574a14
athulcoder <athul.sin.cos.tan@gmail.com>
2025-11-03 17:11:05 +0530

    margin top incread for the counters section to avoid overlap with the herocard in md:devices

--- src/components/AnimatedCounters.jsx
+++ src/components/AnimatedCounters.jsx
@@ -3,7 +3,7 @@ import { counterItems } from "../constants";
 import CountUp from "react-countup";
 const AnimatedCounters = () => {
   return (
-    <div id="counters" className="padding-x-lg xl:mt-0 mt-32 ">
+    <div id="counters" className="padding-x-lg xl:mt-0 mt-32 md:mt-[230px] ">
       <div className="mx-auto grid-4-cols">
         {counterItems.map((item) => (
           <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
