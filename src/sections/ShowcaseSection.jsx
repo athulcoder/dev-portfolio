d2fa781fd4c31f83f90e9b3b6c23ec280a95fd94
athulcoder <athul.sin.cos.tan@gmail.com>
2025-11-23 11:12:20 +0530

    showcase section left side removed from yt

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -18,38 +18,6 @@ const ShowcaseSection = () => {
               />
             );
           })}
-          {/* LEFT */}
-          {/* <div className="first-project-wrapper">
-            <div className="image-wrapper">
-              <img src="/images/project1.png" alt="HRAS" />
-            </div>
-            <div className="text-content">
-              <h2>
-                Hostel Room Allotment System Software that can manage the
-                student allotment proccess in an effeicent way
-              </h2>
-              <p className="text-white-50 md:text-xl">
-                Software built with Java, Swing & SQLite as Database.
-              </p>
-            </div>
-          </div> */}
-
-          {/* RIGHT */}
-          {/* <div className="project-list-wrapper overflow-hidden">
-            <div className="project">
-              <div className="image-wrapper bg-[#ffefdb]">
-                <img src="/images/project2.png" alt="DailyDone" />
-              </div>
-              <h2>DailyDone a Task Managment Platform </h2>
-            </div>
-
-            <div className="project">
-              <div className="image-wrapper bg-[#0872af78]">
-                <img src="/images/project3.png" alt="DailyDone" />
-              </div>
-              <h2>Cinequest a movie search platform </h2>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
