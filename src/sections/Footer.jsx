fbbd345c2162cef0aba2f30b09715c559ab72e6a
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-04 16:32:48 +0530

    new

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,40 +5,42 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <section className="bg-black w-full h-[200px] p-3">
-        <div className="">
-          <h1 className="font-playwrite text-xl">Athul Sabu</h1>
+      <h1 className="font-playwrite text-3xl  text-accent-foreground pl-6">
+        Athul Sabu
+      </h1>
 
-          <ul className="text-sm flex flex-row justify-around h-[100px] items-center">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#work">Works</a>
-            </li>
-            <li>
-              <a href="#">Skills</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
+      <section className="bg-black w-full h-[200px] p-3 min-sm:flex">
+        <ul className="text-sm flex flex-row justify-around h-[100px] items-center min-sm:flex-col min-sm:w-[50%]">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#work">Works</a>
+          </li>
+          <li>
+            <a href="#">Skills</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
 
-        <div>
-          <ul className="text-sm flex flex-row justify-around h-[100px] items-center">
-            {socialLinks.map(({ label, icon: Icon, url }) => {
-              return (
-                <li key={label}>
-                  <a href={url}>
-                    <Icon />
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <ul className="text-sm flex flex-row justify-around h-[100px] items-center min-sm:flex-col">
+          {socialLinks.map(({ label, icon: Icon, url }) => {
+            return (
+              <li key={label}>
+                <a href={url}>
+                  <Icon size={30} />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </section>
+
+      <p className="flex items-center justify-center m-[30px] text-gray-400">
+        Designed & developed with ❤️ by Athul
+      </p>
     </>
   );
 };
